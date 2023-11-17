61d01ff2a58617fea1bcc58ea4f2167fccd39a94
LordM4rcus <luis.paucar.r@tecsup.edu.pe>
2023-11-17 10:59:09 -0500

    centinela ejercicios

---
+++ src/centinela/Centinela.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package centinela;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Centinela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //primero poder ingresar palabras por pantalla
+        Scanner dataEntrante = new Scanner(System.in);
+        
+        boolean salir = false;
+        //mostrar por pantalla las palabras ingresadas
+        System.out.println("ingresar palabra");
+        /*while (salir == false) {            
+            
+            if (!"salir".equals(palabra)) {
+                System.out.println(palabra);
+            } 
+            else {
+                salir = true;
+            }
+        }*/
+        String palabra = ""; 
+         while (!palabra.equals("salir")) {
+             palabra = dataEntrante.nextLine();
+             System.out.println("la palabra es: " + palabra);
+        }
+    }
+    
+}
